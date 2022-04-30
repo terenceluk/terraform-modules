188b7ec21a5c4cf83a0814a00b283419fe20818e
Terence Luk <terence.luk@gmail.com>
2022-04-30 17:59:01 -0400

    Updated
    Updating files

--- Modules/ACR/variables.tf
+++ Modules/ACR/variables.tf
@@ -4,6 +4,8 @@ This file contains all the variables defined for the terraform code
 
 # Variables for Azure Container Registry
 variable "acr_name" {}
+variable "acr_sku" {}
+variable "acr_admin_enabled" {}
 
 # Variables for Management Lock
 variable "acr_lock_name" {}

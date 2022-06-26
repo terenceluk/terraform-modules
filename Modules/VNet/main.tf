4bdcffb39b505ea914f69fc00817f7e7d4e4e9d3
Terence Luk <terence.luk@gmail.com>
2022-06-26 11:14:25 -0400

    Updated
    Added employ subnet emply slice

--- Modules/VNet/main.tf
+++ Modules/VNet/main.tf
@@ -14,6 +14,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.vnet_address_space
   location            = azurerm_resource_group.vnet_rg.location
   resource_group_name = azurerm_resource_group.vnet_rg.name
+  subnet = []
   tags                = var.tags
 }
 
